25594e86390fa982955c9fa42ae63e0192995a73
Vladislav Foss <vladislf@uio.no>
2025-09-28 14:29:59 +0200

    Ny .venv

--- Project1/project1_d.py
+++ Project1/project1_d.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scikit-learn import 
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures
