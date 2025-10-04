764b61672629398e1104970cc677367cdcaef95a
komandoyoko <komandoyoko>
2025-10-04 23:28:24 +0200

    e

--- Project1/Project1_e.py
+++ Project1/Project1_e.py
@@ -1,12 +1,12 @@
 import numpy as np
 from sklearn.metrics import mean_squared_error
-from project1_d import ols_gradient, ridge_gradient, momentum_gd, ADAgrad, RMSProp, ADAM
+from project1_d import ADAgrad, RMSProp, ADAM
 
 def lasso_gradient(X, y, theta, lam):
     n_samples = X.shape[0]
     grad = (2 / n_samples) * X.T @ (X @ theta - y)
     subgrad = lam * np.sign(theta)
-    subgrad[0] = 0.0  # do not penalize intercept
+    subgrad[0] = 0.0
     return grad + subgrad
 
 def run_optimizer_lasso(method, X, y, lam=0.01):
@@ -14,10 +14,20 @@ def run_optimizer_lasso(method, X, y, lam=0.01):
     n_steps = 0.01
     momentum = 0.9
 
-    if method == "GD":
-        return momentum_gd(X, y, iterations, 0, n_steps, "Lasso", lam)
-    elif method == "Momentum":
-        return momentum_gd(X, y, iterations, momentum, n_steps, "Lasso", lam)
+    if method in ["GD", "Momentum"]:
+        theta = np.zeros(X.shape[1])
+        change = np.zeros_like(theta)
+        mse_val = np.zeros(iterations)
+        mom = 0 if method == "GD" else momentum
+
+        for i in range(iterations):
+            grad = lasso_gradient(X, y, theta, lam)
+            change = mom * change + n_steps * grad
+            theta -= change
+            mse_val[i] = mean_squared_error(y, X @ theta)
+
+        return theta, mse_val
+
     elif method == "Adagrad":
         return ADAgrad(X, y, iterations, n_steps, "Lasso", lam)
     elif method == "RMSProp":
@@ -26,6 +36,8 @@ def run_optimizer_lasso(method, X, y, lam=0.01):
         return ADAM(X, y, iterations, n_steps, "Lasso", lam)
     else:
         raise ValueError("Unknown method")
+
+
 
 
 
