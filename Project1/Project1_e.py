80b361196ae876316515de3c66f941aa5ec3d2e7
komandoyoko <komandoyoko>
2025-10-04 23:04:10 +0200

    måtte importere i e og main

--- Project1/Project1_e.py
+++ Project1/Project1_e.py
@@ -1,35 +1,19 @@
+import numpy as np
+from sklearn.metrics import mean_squared_error
+from project1_d import ols_gradient, ridge_gradient, momentum_gd, ADAgrad, RMSProp, ADAM
+
 def lasso_gradient(X, y, theta, lam):
+    n_samples = X.shape[0]
     grad = (2 / n_samples) * X.T @ (X @ theta - y)
     subgrad = lam * np.sign(theta)
     subgrad[0] = 0.0  # do not penalize intercept
     return grad + subgrad
 
-
-def momentum_gd(X, y, iterations, momentum, n_steps, func, lam=0.01):
-    theta = np.zeros(X.shape[1])
-    change = np.zeros_like(theta)
-    mse_val = np.zeros(iterations)
-    
-    for i in range(iterations):
-        if func == "OLS":
-            grad = ols_gradient(X, y, theta)
-        elif func == "Ridge":
-            grad = ridge_gradient(X, y, theta, lam)
-        elif func == "Lasso":
-            grad = lasso_gradient(X, y, theta, lam)
-        else:
-            raise ValueError("Unknown func type")
-
-        change = momentum * change + n_steps * grad
-        theta -= change
-        mse_val[i] = mean_squared_error(y, X @ theta)
-    
-    return theta, mse_val
-
 def run_optimizer_lasso(method, X, y, lam=0.01):
     iterations = 400
     n_steps = 0.01
     momentum = 0.9
+
     if method == "GD":
         return momentum_gd(X, y, iterations, 0, n_steps, "Lasso", lam)
     elif method == "Momentum":
@@ -43,11 +27,5 @@ def run_optimizer_lasso(method, X, y, lam=0.01):
     else:
         raise ValueError("Unknown method")
 
-
-theta_lasso, mse_lasso = run_optimizer_lasso("GD", X_train, y_train, lam=0.01)
-y_pred_lasso = X_test @ theta_lasso
-
-print("MSE (Lasso, GD):", mean_squared_error(y_test, y_pred_lasso))
-print("R2 (Lasso, GD):", R2_score(y_test, y_pred_lasso))
 
 
